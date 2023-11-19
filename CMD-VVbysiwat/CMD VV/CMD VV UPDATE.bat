10b1afb439631b9affa478e9c82351107e1b6ae9
rraapt66 <146457670+rraapt66@users.noreply.github.com>
2023-11-19 19:04:23 +0700

    CMDVV

---
+++ CMD-VVbysiwat/CMD VV/CMD VV UPDATE.bat
@@ -0,0 +1,8 @@
+:update
+git clone https://github.com/rraapt66/CMD-VVbysiwat.git
+cd CMD-VVbysiwat
+cd CMD VV
+del CMD VV.bat
+ping -n 2 127.0.0.1>nul
+echo update Successful
+ping -n 2 127.0.0.1>nul
