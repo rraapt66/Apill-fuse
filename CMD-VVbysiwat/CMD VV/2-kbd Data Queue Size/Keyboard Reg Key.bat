9037025f831da5d332882809ab1679ed91188401
rraapt66 <146457670+rraapt66@users.noreply.github.com>
2023-11-21 21:07:32 +0700

    Delete CMD-VVbysiwat/CMD VV/2-kbd Data Queue Size directory
    Signed-off-by: rraapt66 <146457670+rraapt66@users.noreply.github.com>

--- CMD-VVbysiwat/CMD VV/2-kbd Data Queue Size/Keyboard Reg Key.bat
+++
@@ -1,2 +0,0 @@
-REG ADD HKCU\Software\Microsoft\Windows\CurrentVersion\Applets\Regedit /v LastKey /t REG_SZ /d Computer\HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\kbdclass\Parameters /f
-START regedit
