a0cdd2ae05a50036feaee6a07388c72fd60a61fe
rraapt66 <146457670+rraapt66@users.noreply.github.com>
2023-11-21 20:34:48 +0700

    CMDVV
    Signed-off-by: rraapt66 <146457670+rraapt66@users.noreply.github.com>

---
+++ CMD-VVbysiwat/CMD VV/2-kbd Data Queue Size/Keyboard Reg Key.bat
@@ -0,0 +1,2 @@
+REG ADD HKCU\Software\Microsoft\Windows\CurrentVersion\Applets\Regedit /v LastKey /t REG_SZ /d Computer\HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\kbdclass\Parameters /f
+START regedit
