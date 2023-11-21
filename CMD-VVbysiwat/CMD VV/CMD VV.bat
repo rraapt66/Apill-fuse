ab22a5ce4b467e5b9f5dbce2f52c71fc0d269dfb
rraapt66 <146457670+rraapt66@users.noreply.github.com>
2023-11-21 20:49:29 +0700

    Update CMD VV.bat
    Signed-off-by: rraapt66 <146457670+rraapt66@users.noreply.github.com>

--- CMD-VVbysiwat/CMD VV/CMD VV.bat
+++ CMD-VVbysiwat/CMD VV/CMD VV.bat
@@ -1,5 +1,26 @@
 :admin
 echo run admin only
+:: make sure to get admin
+:-------------------------------------
+:: Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+:: If error flag set, we do not have admin.
+:: If no admin, reqeust admin via VBS script
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+    "%temp%\getadmin.vbs"
+    exit /B
+:gotAdmin
+:: delete script after getting admin
+    if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------
 pause
 goto backup
 :backup
