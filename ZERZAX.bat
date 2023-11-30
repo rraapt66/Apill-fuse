1a6beb62078fd1307b01cb34e6d6ac10801da30a
rraapt66 <146457670+rraapt66@users.noreply.github.com>
2023-11-30 19:39:09 +0700

    Update ZERZAX.bat
    Signed-off-by: rraapt66 <146457670+rraapt66@users.noreply.github.com>

--- ZERZAX.bat
+++ ZERZAX.bat
@@ -51,7 +51,7 @@ echo                    [6] recover Backup Notebook only ║ [16] Keyboard
 echo                    [7] shutdown                     ║ [17] Boost NVIDIA         
 echo                    [8] Check ping                   ║ [18] ultra boost cpu       
 echo                    [9] COMPUTER Check               ║ [19] Disable ultra Boostcpu
-echo                    [10] ?                           ║ [20] Low ping              
+echo                    [10] fixed error windows file    ║ [20] Low ping              
 echo                    [21] join discord                ║ [22] intel boost           
 echo                    [23] AMD boost                   ║ [24] cpu low runnig%                                                                                      
 echo                                                 By siwat                                                 
@@ -67,7 +67,7 @@ if %choix%== 6 Goto :Notebook
 if %choix%== 7 Goto :shd
 if %choix%== 8 goto :ping
 if %choix%== 9 goto :cpt
-if %choix%== 10 goto :commingsoon
+if %choix%== 10 goto :fixed
 if %choix%== 11 goto :BACKUP
 if %choix%== 12 goto :fullclean
 if %choix%== 13 goto :power
@@ -84,6 +84,164 @@ if %choix%== 23 goto :AMD
 if %choix%== 24 goto :lowcpu
 (if not /%input% ==1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23
 goto plese)
+:fixed
+@echo off
+cd %systemroot%\system32
+chcp 65001 >nul 2>&1
+Mode 120,30
+:: Blank/Color Character
+for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do rem"') do (set "DEL=%%a" & set "COL=%%b")
+cls
+echo                                            ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+echo                                                   To Windows FIX by Siwat 
+echo                                            ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+echo.
+echo                                                 ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+echo                                                       Press ENTER to continue   
+echo                                                 ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+echo.  
+echo.
+pause > nul
+
+@echo off
+
+cls
+
+:: Repair Windows Script
+
+
+:: Repair Windows Update components
+DISM.exe /Online /Cleanup-image /Restorehealth
+
+:: Repair system files
+sfc /scannow
+
+:: Repair Windows Firewall
+netsh advfirewall reset
+
+
+:: Clear temporary files
+del /F /Q %temp%\*
+
+:: Rebuild icon cache
+ie4uinit.exe -ClearIconCache
+taskkill /IM explorer.exe /F
+DEL "%localappdata%\IconCache.db" /A
+start explorer.exe
+
+:: Re-register Windows Store apps
+powershell -Command "Get-AppXPackage -AllUsers | Foreach {Add-AppxPackage -DisableDevelopmentMode -Register '$($_.InstallLocation)\AppXManifest.xml'}"
+
+:: Re-register Windows system apps
+for /f "delims=" %%G in ('dir /b %windir%\AppxManifest.xml') do (
+    echo Processing: %%G
+    start /w powershell -command "Add-AppxPackage -DisableDevelopmentMode -Register $env:windir\AppxManifest.xml"
+)
+
+:: Fix common Windows issues
+DISM.exe /Online /Cleanup-Image /CheckHealth
+DISM.exe /Online /Cleanup-Image /ScanHealth
+
+
+:: DLL registration
+for %%i in (%windir%\system32\*.dll) do regsvr32.exe /s "%%i"
+for %%i in (%windir%\syswow64\*.dll) do regsvr32.exe /s "%%i"
+
+:: Disk and boot-related repairs
+chkdsk C: /f /r /x
+bootrec /fixmbr
+bootrec /fixboot
+bootrec /rebuildbcd
+
+:: Clear DNS cache
+ipconfig /flushdns
+
+:: Reset Winsock Catalog
+netsh winsock reset
+
+:: Reset TCP/IP stack
+netsh int ip reset
+
+:: Reset network adapters
+netsh winsock reset catalog
+netsh int ipv4 reset reset.log
+netsh int ipv6 reset reset.log
+
+:: Reset Wi-Fi configuration
+netsh int ip reset reset.log
+
+:: Flush ARP cache
+netsh interface ip delete arpcache
+
+:: Reset network settings
+netsh winsock reset all
+netcfg -d
+ipconfig /release
+ipconfig /renew
+
+echo Windows repair tasks completed.
+
+cls
+
+::Finished
+
+cd %systemroot%\system32
+chcp 65001 >nul 2>&1
+cls
+color 9
+
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo                               ███████╗██╗███╗░░██╗██╗░██████╗██╗░░██╗███████╗██████╗░  ██╗██╗██╗
+echo                               ██╔════╝██║████╗░██║██║██╔════╝██║░░██║██╔════╝██╔══██╗  ██║██║██║
+echo                               █████╗░░██║██╔██╗██║██║╚█████╗░███████║█████╗░░██║░░██║  ██║██║██║
+echo                               ██╔══╝░░██║██║╚████║██║░╚═══██╗██╔══██║██╔══╝░░██║░░██║  ╚═╝╚═╝╚═╝
+echo                               ██║░░░░░██║██║░╚███║██║██████╔╝██║░░██║███████╗██████╔╝  ██╗██╗██╗
+echo                               ╚═╝░░░░░╚═╝╚═╝░░╚══╝╚═╝╚═════╝░╚═╝░░╚═╝╚══════╝╚═════╝░  ╚═╝╚═╝╚═╝
+echo.
+echo.
+echo.
+echo.
+echo.
+echo                                               Do you want to Restart your PC ?     
+echo                                       Press [Y] to Restart your PC , Press [N] To EXIT  
+echo.
+echo.
+echo.
+echo.
+
+@ECHO Off
+
+    
+
+    SET /P yesno=Choose Your Option:
+    IF "%yesno%"=="y" GOTO Confirmation
+    IF "%yesno%"=="Y" GOTO Confirmation
+    IF "%yesno%"=="n" GOTO End
+    IF "%yesno%"=="N" GOTO End
+    
+    :Confirmation
+    
+    ECHO System is going to Restart now
+    
+    shutdown /r /t 0 
+    
+    GOTO EOF
+    :End
+    
+    ECHO System Restart cancelled
+    
+    TIMEOUT 5 >nul
+    
+    :EOF
+    exit
 :plese
 cls
 echo please selected
