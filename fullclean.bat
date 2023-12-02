470ea252a2099c0d51bb947cf7432a10eff979ed
rraapt66 <146457670+rraapt66@users.noreply.github.com>
2023-12-02 08:00:13 +0700

    Update fullclean.bat
    Signed-off-by: rraapt66 <146457670+rraapt66@users.noreply.github.com>

--- fullclean.bat
+++ fullclean.bat
@@ -9,8 +9,10 @@ color c
 del C:\Windows\SoftwareDistribution >nul >>APB_Log.txt
 /s /f /q c:\windows\temp\*.* >>APB_Log.txt
 rd /s /q c:\windows\temp >>APB_Log.txt
+ping -n 4 127.0.0.1>nul
 md c:\windows\temp >>APB_Log.txt
 del /s /f /q C:\WINDOWS\Prefetch >>APB_Log.txt
+ping -n 4 127.0.0.1>nul
 del /s /f /q %temp%\*.* >>APB_Log.txt
 rd /s /q %temp%
 md %temp%
@@ -19,59 +21,89 @@ MKDIR C:\Windows\Temp
 takeown /f "C:\Windows\Temp" /r /d y
 takeown /f %temp% /r /d y
 del /q/f/s c:\windows\*.node >NUL >>APB_Log.txt 
+cls
 echo Successful
+ping -n 4 127.0.0.1>nul
 del /q/f/s c:\windows\*. >NUL >>APB_Log.txt 
+cls
 echo Successful
 del /q/f/s c:\windows\*.tmp >NUL >>APB_Log.txt 
+cls
 echo Successful
 deltree /y c:\windows\tempor~1 >NUL >>APB_Log.txt
+cls
+ping -n 4 127.0.0.1>nul
 echo Successful
 deltree /y c:\windows\temp >NUL >>APB_Log.txt
+cls
 echo Successful
 deltree /y c:\windows\tmp >NUL >>APB_Log.txt
+cls
 echo Successful
+ping -n 4 127.0.0.1>nul
 deltree /y c:\windows\ff*.tmp >NUL >>APB_Log.txt
+cls
 echo Successful
 deltree /y c:\windows\history >NUL >>APB_Log.txt
+cls
 echo Successful
+ping -n 4 127.0.0.1>nul
 deltree /y c:\windows\cookies >NUL >>APB_Log.txt
+cls
 echo Successful
 deltree /y c:\windows\recent >NUL >>APB_Log.txt
+cls
 echo Successful
 deltree /y c:\windows\spool\printers >NUL >>APB_Log.txt
-echo Successful
-echo Successful
-del /q/f/s %TEMP%\ >NUL >>APB_Log.txt
+cls
 echo Successful
 del /q/f/s %TEMP%\ >NUL >>APB_Log.txt
+cls
 echo Successful
 del /q/f/s %TEMP%\ >NUL >>APB_Log.txt
+cls
 echo Successful
 del /q/f/s %TEMP%\ >NUL >>APB_Log.txt
+cls
+ping -n 4 127.0.0.1>nul
 echo Successful
 del /q/f/s %TEMP%\ >NUL >>APB_Log.txt
+cls
+ping -n 4 127.0.0.1>nul
 echo Successful
 del /q/f/s %TEMP%\ >NUL >>APB_Log.txt
+cls
 echo Successful
 del /q/f/s %TEMP%\ >NUL >>APB_Log.txt
+cls
 echo Successful
 del /q/f/s %TEMP%\ >NUL >>APB_Log.txt
+cls
 echo Successful
 del /q/f/s %TEMP%\ >NUL >>APB_Log.txt
+cls
 echo Successful
 del /q/f/s %TEMP%\ >NUL >>APB_Log.txt
+cls
+ping -n 4 127.0.0.1>nul
+echo Successful
+del /q/f/s %TEMP%\ >NUL >>APB_Log.txt
+cls
 cleanmgr >>APB_Log.txt
 echo Successful
 cls
 rd /s /q %temp% >NUL >>APB_Log.txt
+cls
 echo Successful
 ping -n 2 127.0.0.1>nul
 cls
 del /q/f/s %TEMP%\*. >NUL >>APB_Log.txt
+cls
 echo Successful....
 ping -n 2 127.0.0.1>nul
 cls
 md %temp% >NUL >>APB_Log.txt
+cls
 echo Successful
 ping -n 2 127.0.0.1>nul
 cls
