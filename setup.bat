38e6a927aa2191f0b36c3dd9637340eb103add51
rraapt66 <146457670+rraapt66@users.noreply.github.com>
2023-12-05 14:01:48 +0700

    fixed error
    Signed-off-by: rraapt66 <146457670+rraapt66@users.noreply.github.com>

--- setup.bat
+++ setup.bat
@@ -2,9 +2,13 @@
 python -m pip install -r requirements.txt
 pause
 start https://github.com/git-for-windows/git/releases/download/v2.43.0.windows.1/Git-2.43.0-64-bit.exe
+start https://www.python.org/ftp/python/3.12.0/python-3.12.0-amd64.exe
 cls
 echo Run Git first
-ping -n 24 127.0.0.1>nul
+ping -n 16 127.0.0.1>nul
+cls 
+echo run python first
+ping -n 16 127.0.0.1>nul
 start ZERZAX.bat
 start Boostfps(dont run is Beta).exe
 exit
