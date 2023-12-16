75c0d022620b3bc7eb061e7de4f85ef05dbf143e
rraapt66 <146457670+rraapt66@users.noreply.github.com>
2023-12-15 23:37:15 -0800

    New Funtcion more 1
    Signed-off-by: rraapt66 <146457670+rraapt66@users.noreply.github.com>

---
+++ Check update.bat
@@ -0,0 +1,42 @@
+@echo off
+echo check update
+echo .
+del APB_Log.txt > nul
+ping -n 4 127.0.0.1>nul
+echo ..
+del error.bat > nul
+ping -n 1 127.0.0.1>nul
+echo ...
+del reportbug.py > nul
+ping -n 3 127.0.0.1>nul
+echo ....
+del requirements.txt > nul
+ping -n 2 127.0.0.1>nul
+echo .....
+del setup.bat > nul
+ping -n 4 127.0.0.1>nul
+echo .............
+del ZERZAX.bat > nul
+del readme.txt > nul
+git clone https://github.com/rraapt66/ZERZAX.git
+echo .
+ping -n 2 127.0.0.1>nul
+cls
+echo ..
+ping -n 3 127.0.0.1>nul
+cls
+echo ...
+ping -n 4 127.0.0.1>nul
+cls
+echo ....
+ping -n 1 127.0.0.1>nul
+cls
+echo .....
+ping -n 2 127.0.0.1>nul
+cls
+echo .............
+cls
+echo exit.......
+ping -n 4 127.0.0.1>nul
+color d
+exit
