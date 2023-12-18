10b5abe91d7f2fc03ce09b945ce9235eecddc8f0
rraapt66 <146457670+rraapt66@users.noreply.github.com>
2023-12-18 22:05:24 +0700

    Update Apill-fuse.bat
    Signed-off-by: rraapt66 <146457670+rraapt66@users.noreply.github.com>

--- Apill-fuse.bat
+++ Apill-fuse.bat
@@ -859,7 +859,7 @@ defrag c: >>APB_Log.txt
 cls
 title clear file
 color c 
-del C:\Windows\SoftwareDistribution >nul >>APB_Log.txt
+del C:\Windows\SoftwareDistribution  >>APB_Log.txt
 /s /f /q c:\windows\temp\*.* >>APB_Log.txt
 rd /s /q c:\windows\temp >>APB_Log.txt
 ping -n 4 127.0.0.1>nul
@@ -877,37 +877,37 @@ del /q/f/s c:\windows\*.node >NUL >>APB_Log.txt
 cls
 echo Successful
 ping -n 4 127.0.0.1>nul
-del /q/f/s c:\windows\*. >NUL >>APB_Log.txt 
+del /q/f/s c:\windows\*. >>APB_Log.txt 
 cls
 echo Successful
-del /q/f/s c:\windows\*.tmp >NUL >>APB_Log.txt 
+del /q/f/s c:\windows\*.tmp  >>APB_Log.txt 
 cls
 echo Successful
-deltree /y c:\windows\tempor~1 >NUL >>APB_Log.txt
+deltree /y c:\windows\tempor~1  >>APB_Log.txt
 cls
 ping -n 4 127.0.0.1>nul
 echo Successful
-deltree /y c:\windows\temp >NUL >>APB_Log.txt
+deltree /y c:\windows\temp  >>APB_Log.txt
 cls
 echo Successful
-deltree /y c:\windows\tmp >NUL >>APB_Log.txt
-cls
-echo Successful
-ping -n 4 127.0.0.1>nul
-deltree /y c:\windows\ff*.tmp >NUL >>APB_Log.txt
-cls
-echo Successful
-deltree /y c:\windows\history >NUL >>APB_Log.txt
+deltree /y c:\windows\tmp >>APB_Log.txt
 cls
 echo Successful
 ping -n 4 127.0.0.1>nul
-deltree /y c:\windows\cookies >NUL >>APB_Log.txt
+deltree /y c:\windows\ff*.tmp  >>APB_Log.txt
 cls
 echo Successful
-deltree /y c:\windows\recent >NUL >>APB_Log.txt
+deltree /y c:\windows\history  >>APB_Log.txt
 cls
 echo Successful
-deltree /y c:\windows\spool\printers >NUL >>APB_Log.txt
+ping -n 4 127.0.0.1>nul
+deltree /y c:\windows\cookies >>APB_Log.txt
+cls
+echo Successful
+deltree /y c:\windows\recent >>APB_Log.txt
+cls
+echo Successful
+deltree /y c:\windows\spool\printers >>APB_Log.txt
 cls
 @echo off
 title Defrag Driv c
@@ -935,85 +935,85 @@ del /q/f/s c:\windows\*.node >NUL >>APB_Log.txt
 cls
 echo Successful
 ping -n 4 127.0.0.1>nul
-del /q/f/s c:\windows\*. >NUL >>APB_Log.txt 
+del /q/f/s c:\windows\*.  >>APB_Log.txt 
 cls
 echo Successful
-del /q/f/s c:\windows\*.tmp >NUL >>APB_Log.txt 
+del /q/f/s c:\windows\*.tmp  >>APB_Log.txt 
 cls
 echo Successful
-deltree /y c:\windows\tempor~1 >NUL >>APB_Log.txt
-cls
-ping -n 4 127.0.0.1>nul
-echo Successful
-deltree /y c:\windows\temp >NUL >>APB_Log.txt
-cls
-echo Successful
-deltree /y c:\windows\tmp >NUL >>APB_Log.txt
-cls
-echo Successful
-ping -n 4 127.0.0.1>nul
-deltree /y c:\windows\ff*.tmp >NUL >>APB_Log.txt
-cls
-echo Successful
-deltree /y c:\windows\history >NUL >>APB_Log.txt
-cls
-echo Successful
-ping -n 4 127.0.0.1>nul
-deltree /y c:\windows\cookies >NUL >>APB_Log.txt
-cls
-echo Successful
-deltree /y c:\windows\recent >NUL >>APB_Log.txt
-cls
-echo Successful
-deltree /y c:\windows\spool\printers >NUL >>APB_Log.txt
-cls
-echo Successful
-del /q/f/s %TEMP%\ >NUL >>APB_Log.txt
-cls
-echo Successful
-del /q/f/s %TEMP%\ >NUL >>APB_Log.txt
-cls
-echo Successful
-del /q/f/s %TEMP%\ >NUL >>APB_Log.txt
+deltree /y c:\windows\tempor~1  >>APB_Log.txt
 cls
 ping -n 4 127.0.0.1>nul
 echo Successful
-del /q/f/s %TEMP%\ >NUL >>APB_Log.txt
+deltree /y c:\windows\temp  >>APB_Log.txt
+cls
+echo Successful
+deltree /y c:\windows\tmp  >>APB_Log.txt
+cls
+echo Successful
+ping -n 4 127.0.0.1>nul
+deltree /y c:\windows\ff*.tmp >>APB_Log.txt
+cls
+echo Successful
+deltree /y c:\windows\history >>APB_Log.txt
+cls
+echo Successful
+ping -n 4 127.0.0.1>nul
+deltree /y c:\windows\cookies >>APB_Log.txt
+cls
+echo Successful
+deltree /y c:\windows\recent >>APB_Log.txt
+cls
+echo Successful
+deltree /y c:\windows\spool\printers >>APB_Log.txt
+cls
+echo Successful
+del /q/f/s %TEMP%\ >>APB_Log.txt
+cls
+echo Successful
+del /q/f/s %TEMP%\ >>APB_Log.txt
+cls
+echo Successful
+del /q/f/s %TEMP%\ >>APB_Log.txt
 cls
 ping -n 4 127.0.0.1>nul
 echo Successful
-del /q/f/s %TEMP%\ >NUL >>APB_Log.txt
-cls
-echo Successful
-del /q/f/s %TEMP%\ >NUL >>APB_Log.txt
-cls
-echo Successful
-del /q/f/s %TEMP%\ >NUL >>APB_Log.txt
-cls
-echo Successful
-del /q/f/s %TEMP%\ >NUL >>APB_Log.txt
-cls
-echo Successful
-del /q/f/s %TEMP%\ >NUL >>APB_Log.txt
+del /q/f/s %TEMP%\ >>APB_Log.txt
 cls
 ping -n 4 127.0.0.1>nul
 echo Successful
-del /q/f/s %TEMP%\ >NUL >>APB_Log.txt
+del /q/f/s %TEMP%\ >>APB_Log.txt
+cls
+echo Successful
+del /q/f/s %TEMP%\ >>APB_Log.txt
+cls
+echo Successful
+del /q/f/s %TEMP%\ >>APB_Log.txt
+cls
+echo Successful
+del /q/f/s %TEMP%\ >>APB_Log.txt
+cls
+echo Successful
+del /q/f/s %TEMP%\ >>APB_Log.txt
+cls
+ping -n 4 127.0.0.1>nul
+echo Successful
+del /q/f/s %TEMP%\ >>APB_Log.txt
 cls
 cleanmgr >>APB_Log.txt
 echo Successful
 cls
-rd /s /q %temp% >NUL >>APB_Log.txt
+rd /s /q %temp% >>APB_Log.txt
 cls
 echo Successful
 ping -n 2 127.0.0.1>nul
 cls
-del /q/f/s %TEMP%\*. >NUL >>APB_Log.txt
+del /q/f/s %TEMP%\*. >>APB_Log.txt
 cls
 echo Successful....
 ping -n 2 127.0.0.1>nul
 cls
-md %temp% >NUL >>APB_Log.txt
+md %temp%  >>APB_Log.txt
 tree
 cls
 echo Successful
