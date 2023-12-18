14a6c74c4fc9e70eeb423ed182d98959b8c19054
rraapt66 <146457670+rraapt66@users.noreply.github.com>
2023-12-18 16:57:38 +0700

    Update setup.bat
    Signed-off-by: rraapt66 <146457670+rraapt66@users.noreply.github.com>

--- setup.bat
+++ setup.bat
@@ -2,10 +2,10 @@
 start https://github.com/git-for-windows/git/releases/download/v2.43.0.windows.1/Git-2.43.0-64-bit.exe
 start https://www.python.org/ftp/python/3.12.0/python-3.12.0-amd64.exe
 echo Run Git first
-ping -n 16 127.0.0.1>nul
+ping -n 24 127.0.0.1>nul
 cls 
 echo run python first
-ping -n 16 127.0.0.1>nul
+ping -n 29 127.0.0.1>nul
 python -m pip install -r requirements.txt
 pause
 cls
