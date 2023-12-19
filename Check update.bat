4fcedbeec0338c4fb49c5ad272bb19d3aeb01169
rraapt66 <146457670+rraapt66@users.noreply.github.com>
2023-12-19 20:19:46 +0700

    Add files via upload
    Signed-off-by: rraapt66 <146457670+rraapt66@users.noreply.github.com>

--- Check update.bat
+++ Check update.bat
@@ -7,6 +7,8 @@ del APB_Log.txt > nul
 ping -n 4 127.0.0.1>nul
 echo ..
 del error.bat > nul
+del setup-spotify.bat > nul
+del spot > nul
 ping -n 1 127.0.0.1>nul
 echo ...
 ping -n 3 127.0.0.1>nul
@@ -14,6 +16,7 @@ echo ....
 ping -n 2 127.0.0.1>nul
 echo .....
 del setup.bat > nul
+del Thanks-for-Dowload.txt > nul
 ping -n 4 127.0.0.1>nul
 echo .............
 del Apill-fuse.bat > nul
