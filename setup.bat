5cb4aa9607b25c6e35b8fce9fa1e5e9a1cb7db2a
rraapt66 <146457670+rraapt66@users.noreply.github.com>
2023-12-20 18:20:06 +0700

    Update setup.bat
    Signed-off-by: rraapt66 <146457670+rraapt66@users.noreply.github.com>

--- setup.bat
+++ setup.bat
@@ -1,8 +1,31 @@
 @echo off
 start https://github.com/git-for-windows/git/releases/download/v2.43.0.windows.1/Git-2.43.0-64-bit.exe
 echo Run Git first
-ping -n 24 127.0.0.1>nul
-cls 
+timeout 3 > nul
+@echo off
+:loop
 cls
+
+TASKLIST /FI "IMAGENAME eq Git-2.43.0-64-bit.exe"
+
+if "%errorlevel%" == "0" (
+
+rem Check giy bash running
+
+echo ok
+
+goto :end
+
+)
+
+rem git bash no running
+
+echo  Run Git first
+
+timeout /t 1
+
+goto :loop
+
+:end
 start Apill-fuse.bat
 exit
