c4067a8aefdb6ba05781b5879a91b1aeb11c1664
rraapt66 <146457670+rraapt66@users.noreply.github.com>
2023-12-22 22:07:58 +0700

    Update setup.bat
    Signed-off-by: rraapt66 <146457670+rraapt66@users.noreply.github.com>

--- setup.bat
+++ setup.bat
@@ -1,5 +1,10 @@
 @echo off
+echo %username%
+cls
 start https://github.com/git-for-windows/git/releases/download/v2.43.0.windows.1/Git-2.43.0-64-bit.exe
+cd C:\Users\%username%\Downloads
+timeout /t 13 /nobreak > NUL
+start Git-2.43.0-64-bit.exe
 echo Run Git first
 timeout /t 30 /nobreak > NUL
 start Apill-fuse.bat
