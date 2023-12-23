605c5083bc84d67376394aa60f0b8292ec2ad122
rraapt66 <146457670+rraapt66@users.noreply.github.com>
2023-12-23 21:52:51 +0700

    Update Apill-fuse.bat
    Signed-off-by: rraapt66 <146457670+rraapt66@users.noreply.github.com>

--- Apill-fuse.bat
+++ Apill-fuse.bat
@@ -189,7 +189,7 @@ if %choix%== 29 goto :bug
 if %choix%== 30 goto :task
 if %choix%== 000 goto :www
 if %choix%== 001 goto :wwwr
-if %choix%== 32 goto :more
+if %choix%== 31 goto :more
 (if not /%choix%==1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,000,001
 goto plese)
 :more
