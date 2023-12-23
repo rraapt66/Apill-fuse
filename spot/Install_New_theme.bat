ca9e5b2c8065d8b3a96ef80893b6443dc522d0e8
rraapt66 <146457670+rraapt66@users.noreply.github.com>
2023-12-23 21:48:12 +0700

    Delete spot directory
    Signed-off-by: rraapt66 <146457670+rraapt66@users.noreply.github.com>

--- spot/Install_New_theme.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd c:
-%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\powershell.exe -Command "&{[Net.ServicePointManager]::SecurityProtocol = 3072}; """"& { $(Invoke-WebRequest -UseBasicParsing 'https://spotx-official.github.io/run.ps1')} -new_theme """" | Invoke-Expression"
-
-pause
-exit /b
