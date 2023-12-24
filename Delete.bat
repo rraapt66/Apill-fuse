cfcbbc62884d65532df66114cbaac3ac98875f6f
rraapt66 <146457670+rraapt66@users.noreply.github.com>
2023-12-24 17:36:47 +0700

    Update Delete.bat
    Signed-off-by: rraapt66 <146457670+rraapt66@users.noreply.github.com>

--- Delete.bat
+++ Delete.bat
@@ -3,8 +3,7 @@ del use.txt> nul
 del Thanks-for-Dowload.txt > nul
 del readme.txt > nul
 del setup.bat > nul
-del Apill-fuse.bat > nul
+del Apill-fuse.bat >nul
 cd C:\Users\%username%
-del user-for-Apill-fuse > nul
 echo x=msgbox("Bye see you again" ,0, "max") >>bye.vbs
 start bye.vbs 
