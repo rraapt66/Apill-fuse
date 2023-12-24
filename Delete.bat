dc2745351d2a9272ae64aaa97de3411bdf3c8f16
rraapt66 <146457670+rraapt66@users.noreply.github.com>
2023-12-24 15:49:14 +0700

    Add files via upload
    Signed-off-by: rraapt66 <146457670+rraapt66@users.noreply.github.com>

---
+++ Delete.bat
@@ -0,0 +1,10 @@
+@echo off
+del use.txt
+del Thanks-for-Dowload.txt
+del readme.txt
+del setup.bat
+del Apill-fuse.bat
+cd C:\Users\%username%
+del user-for-Apill-fuse
+echo x=msgbox("Bye see you again" ,0, "max") >>bye.vbs
+start bye.vbs 
