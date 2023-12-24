080f62dc791cdc8ff850d02d880accd5eb482db3
rraapt66 <146457670+rraapt66@users.noreply.github.com>
2023-12-24 15:54:36 +0700

    Update Apill-fuse.bat
    Signed-off-by: rraapt66 <146457670+rraapt66@users.noreply.github.com>

--- Apill-fuse.bat
+++ Apill-fuse.bat
@@ -2161,38 +2161,38 @@ echo Cleaning PC...
 ping -n 4.5 127.0.0.1>nul
 @echo off
 del /q/f/s %TEMP%\* >>APB_Log.txt
-del /s /f /q c:\windows\temp. > APB_Log.txt
-del /s /f /q C:\WINDOWS\Prefetch > APB_Log.txt
+del /s /f /q c:\windows\temp. >>APB_Log.txt
+del /s /f /q C:\WINDOWS\Prefetch >>APB_Log.txt
 del /s /f /q %temp%. >> APB_Log.txt
-del /s /f /q %systemdrive%\*.tmp >APB_Log.txt
-del /s /f /q %systemdrive%\*._mp >APB_Log.txt
-del /s /f /q %systemdrive%\*.log >APB_Log.txt
-del /s /f /q %systemdrive%\*.gid >APB_Log.txt
-del /s /f /q %systemdrive%\*.chk >APB_Log.txt
-del /s /f /q %systemdrive%\*.old >APB_Log.txt
-del /s /f /q %systemdrive%\recycled\*.* >APB_Log.txt
-del /s /f /q %systemdrive%\$Recycle.Bin\*.* >APB_Log.txt
-del /s /f /q %windir%\*.bak >APB_Log.txt
-del /s /f /q %windir%\prefetch\*.* >APB_Log.txt
+del /s /f /q %systemdrive%\*.tmp >>APB_Log.txt
+del /s /f /q %systemdrive%\*._mp >>APB_Log.txt
+del /s /f /q %systemdrive%\*.log >>APB_Log.txt
+del /s /f /q %systemdrive%\*.gid >>APB_Log.txt
+del /s /f /q %systemdrive%\*.chk >>APB_Log.txt
+del /s /f /q %systemdrive%\*.old >>APB_Log.txt
+del /s /f /q %systemdrive%\recycled\*.* >>APB_Log.txt
+del /s /f /q %systemdrive%\$Recycle.Bin\*.* >>APB_Log.txt
+del /s /f /q %windir%\*.bak >>APB_Log.txt
+del /s /f /q %windir%\prefetch\*.* >>APB_Log.txt
 del /s /f /q %LocalAppData%\Microsoft\Windows\Explorer\thumbcache_*.db >APB_Log.txt
 del /s /f /q %LocalAppData%\Microsoft\Windows\Explorer\*.db >APB_Log.txt
-del /f /q %SystemRoot%\Logs\CBS\CBS.log >APB_Log.txt
-del /f /q %SystemRoot%\Logs\DISM\DISM.log >APB_Log.txt
+del /f /q %SystemRoot%\Logs\CBS\CBS.log >>APB_Log.txt
+del /f /q %SystemRoot%\Logs\DISM\DISM.log >>APB_Log.txt
 takeown /f "C:\Windows\Temp" /r /d y
-del /q/f/s %TEMP%\* >APB_Log.txt
-del /s /f /q c:\windows\temp. > APB_Log.txt
-del /s /f /q C:\WINDOWS\Prefetch > APB_Log.txt
-del /s /f /q %temp%. > APB_Log.txt
-del /s /f /q %systemdrive%\*.tmp >APB_Log.txt
-del /s /f /q %systemdrive%\*._mp >APB_Log.txt
-del /s /f /q %systemdrive%\*.log >APB_Log.txt
-del /s /f /q %systemdrive%\*.gid >APB_Log.txt
-del /s /f /q %systemdrive%\*.chk >APB_Log.txt
-del /s /f /q %systemdrive%\*.old >APB_Log.txt
-del /s /f /q %systemdrive%\recycled\*.* >APB_Log.txt
-del /s /f /q %systemdrive%\$Recycle.Bin\*.* >APB_Log.txt
+del /q/f/s %TEMP%\* >>APB_Log.txt
+del /s /f /q c:\windows\temp. >> APB_Log.txt
+del /s /f /q C:\WINDOWS\Prefetch >> APB_Log.txt
+del /s /f /q %temp%. >> APB_Log.txt
+del /s /f /q %systemdrive%\*.tmp >>APB_Log.txt
+del /s /f /q %systemdrive%\*._mp >>APB_Log.txt
+del /s /f /q %systemdrive%\*.log >>APB_Log.txt
+del /s /f /q %systemdrive%\*.gid >>APB_Log.txt
+del /s /f /q %systemdrive%\*.chk >>APB_Log.txt
+del /s /f /q %systemdrive%\*.old >>APB_Log.txt
+del /s /f /q %systemdrive%\recycled\*.* >>APB_Log.txt
+del /s /f /q %systemdrive%\$Recycle.Bin\*.* >>APB_Log.txt
 del /s /f /q %windir%\*.bak >APB_Log.txt
-del /s /f /q %windir%\prefetch\*.* >APB_Log.txt
+del /s /f /q %windir%\prefetch\*.* >>APB_Log.txt
 del /s /f /q %LocalAppData%\Microsoft\Windows\Explorer\thumbcache_*.db >>APB_Log.txt
 del /s /f /q %LocalAppData%\Microsoft\Windows\Explorer\*.db >>APB_Log.txt
 del /f /q %SystemRoot%\Logs\CBS\CBS.log >>APB_Log.txt
