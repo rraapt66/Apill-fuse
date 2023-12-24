cdeebd79a761440ed84d6cdce86b756596200ea9
rraapt66 <146457670+rraapt66@users.noreply.github.com>
2023-12-24 21:23:18 +0700

    Update Apill-fuse.bat
    Signed-off-by: rraapt66 <146457670+rraapt66@users.noreply.github.com>

--- Apill-fuse.bat
+++ Apill-fuse.bat
@@ -111,8 +111,7 @@ if "%userID%"=="%id%" (
 echo x=msgbox("your ID correct" ,0, "max") >> IDS.vbs
 del IDS.vbs
 echo x=msgbox("your ID correct" ,0, "max") >> IDS.vbs
-start IDS.vbs
-goto kill
+start IDS.vbs (goto kill)
     rem ทำสิ่งที่คุณต้องการทำต่อ
 ) else (
 echo x=msgbox("your ID correct" ,0, "max") >> IDS.vbs
