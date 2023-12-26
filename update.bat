bcef188d8909d94c0e0294493a1d4c5f06a7f907
rraapt66 <146457670+rraapt66@users.noreply.github.com>
2023-12-26 18:23:19 +0700

    ADD UPDATE
    Signed-off-by: rraapt66 <146457670+rraapt66@users.noreply.github.com>

---
+++ update.bat
@@ -0,0 +1,15 @@
+@echo off
+del use.txt> nul
+del Thanks-for-Dowload.txt > nul
+del readme.txt > nul
+del setup.bat > nul
+del Apill-fuse.bat >nul
+del Delete.bat > nul
+echo x=msgbox("update Successful" ,0, "max") >>update.vbs
+start update.vbs 
+git clone https://github.com/rraapt66/Apill-fuse.git
+cd Apill-fuse-ultra
+start Apill-fuse.abt
+del update.vbs
+attrib +h update.bat
+exit
