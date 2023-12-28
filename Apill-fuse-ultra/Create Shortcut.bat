02296e7f4c076a1601e154bb6954c106fa7bc2e7
rraapt66 <146457670+rraapt66@users.noreply.github.com>
2023-12-28 16:50:34 +0700

    Update Create Shortcut.bat
    Signed-off-by: rraapt66 <146457670+rraapt66@users.noreply.github.com>

--- Apill-fuse-ultra/Create Shortcut.bat
+++ Apill-fuse-ultra/Create Shortcut.bat
@@ -1,3 +1,4 @@
+:setupss
 @echo off
 rd /s /q Apill-fuse
 )
@@ -25,9 +26,28 @@ del CreateShortcut.vbs
 start C:\Users\%username%\Apill-fuse-ultra\Apill-fuse-ultra\Apill-fuse-ultra\Apill-fuse\setup.bat
 exit
 :startSetup
-(color c
-rem ส่วนที่จะทำงานเมื่อไม่มี C:\Program Files\Git
-echo Starting setup from alternative location...
-rem สร้างเงื่อนไขเพิ่มเติมตามที่คุณต้องการ
-start C:\Users\%username%\Apill-fuse\Apill-fuse-ultra\Apill-fuse\setup.bat exit
-cls)
+@echo off
+echo %username%
+cls
+start https://github.com/git-for-windows/git/releases/download/v2.43.0.windows.1/Git-2.43.0-64-bit.exe > nul 
+echo setup......
+:check
+set programPath=C:\Users\%username%\Downloads\Git-2.43.0-64-bit.exe
+REM ตรวจสอบว่าโปรแกรมมีอยู่หรือไม่
+IF EXIST "%programPath%" (
+    REM เปิดโปรแกร
+    START "" "%programPath%"
+	taskkill /F /IM chrome.exe > nul
+) ELSE (
+    REM ถ้าไม่พบโปรแกรม
+goto check
+)
+@echo off
+:checkRunning
+@echo off
+tasklist | find /i "Git-2.43.0-64-bit.exe" > nul
+if %errorlevel% neq 0 goto start
+goto checkRunning
+
+:start
+goto setupss
