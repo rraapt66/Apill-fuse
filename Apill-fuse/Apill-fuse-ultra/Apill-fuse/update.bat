1c6b1c71022e34b949e255703acc7a8ef14f39c2
rraapt66 <146457670+rraapt66@users.noreply.github.com>
2023-12-28 18:15:00 +0700

    Add files via upload
    Signed-off-by: rraapt66 <146457670+rraapt66@users.noreply.github.com>

---
+++ Apill-fuse/Apill-fuse-ultra/Apill-fuse/update.bat
@@ -0,0 +1,17 @@
+@echo off
+del use.txt> nul
+del Thanks-for-Dowload.txt > nul
+del readme.txt > nul
+del setup.bat > nul
+del Apill-fuse.bat >nul
+del Delete.bat > nul
+attrib +h update.bat
+git clone https://github.com/rraapt66/Apill-fuse.git
+cd Apill-fuse
+echo x=msgbox("update Successful" ,0, "max") >>update.vbs
+start update.vbs 
+cls
+echo enter to exit
+pause
+del update.vbs
+exit
