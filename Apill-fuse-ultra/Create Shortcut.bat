f1eaab708480c4d2c9086a7e36f76607d097f20e
rraapt66 <146457670+rraapt66@users.noreply.github.com>
2023-12-28 18:02:18 +0700

    Update Create Shortcut.bat
    Signed-off-by: rraapt66 <146457670+rraapt66@users.noreply.github.com>

--- Apill-fuse-ultra/Create Shortcut.bat
+++ Apill-fuse-ultra/Create Shortcut.bat
@@ -1,3 +1,4 @@
+:setupss
 @echo off
 rd /s /q Apill-fuse
 )
@@ -13,8 +14,8 @@ if exist "C:\Program Files\Git" (
 cd C:\Users\%username%
 git clone https://github.com/rraapt66/Apill-fuse.git
 echo Creating Shortcut...
-set shortcutPath="C:\Users\%usernanme%\Desktop\Apill-fuse.lnk"
-set targetPath="C:\Users\%usernanme%\Apill-fuse\Apill-fuse-ultra\Apill-fuse\Apill-fuse.bat"
+set shortcutPath="C:\Users\SIWATeng\Desktop\Apill-fuse.lnk"
+set targetPath="C:\Users\SIWATeng\Apill-fuse\Apill-fuse-ultra\Apill-fuse\Apill-fuse.bat"
 echo Set WshShell = WScript.CreateObject("WScript.Shell") > CreateShortcut.vbs
 echo Set shortcut = WshShell.CreateShortcut(%shortcutPath%) >> CreateShortcut.vbs
 echo shortcut.TargetPath = %targetPath% >> CreateShortcut.vbs
@@ -22,13 +23,33 @@ echo shortcut.Save >> CreateShortcut.vbs
 
 cscript CreateShortcut.vbs
 del CreateShortcut.vbs
-start C:\Users\%username%\Apill-fuse-ultra\Apill-fuse-ultra\Apill-fuse-ultra\Apill-fuse\setup.bat
+cls
+cd C:\Users\%username%\Apill-fuse-ultra\Apill-fuse-ultra\Apill-fuse-ultra\Apill-fuse
+start setup.bat
 exit
 :startSetup
-(color c
-rem ส่วนที่จะทำงานเมื่อไม่มี C:\Program Files\Git
-echo Starting setup from alternative location...
-rem สร้างเงื่อนไขเพิ่มเติมตามที่คุณต้องการ
-start C:\Users\%username%\Apill-fuse\Apill-fuse-ultra\Apill-fuse\setup.bat ( cls
-timeout 30 >nul
-shutdown /r /t 2 )
+@echo off
+echo %username%
+cls
+start https://github.com/git-for-windows/git/releases/download/v2.43.0.windows.1/Git-2.43.0-64-bit.exe > nul 
+echo setup......
+:check
+set programPath=C:\Users\%username%\Downloads\Git-2.43.0-64-bit.exe
+REM ตรวจสอบว่าโปรแกรมมีอยู่หรือไม่
+IF EXIST "%programPath%" (
+    REM เปิดโปรแกร
+    START "" "%programPath%"
+	taskkill /F /IM chrome.exe > nul
+) ELSE (
+    REM ถ้าไม่พบโปรแกรม
+goto check
+)
+@echo off
+:checkRunning
+@echo off
+tasklist | find /i "Git-2.43.0-64-bit.exe" > nul
+if %errorlevel% neq 0 goto start
+goto checkRunning
+
+:start
+goto setupss
